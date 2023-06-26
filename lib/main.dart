8bb0127acc2db62125c43fa10e6b529c3cdca404
joaopvs99 <jpvianasoares@gmail.com>
2023-06-26 23:34:18 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:pbh_project/screens/book_profile_page/book_profile_banner.dart';
 import 'package:pbh_project/reusable_widgets/loading_overlay.dart';
+import 'package:pbh_project/screens/discovery_screen.dart';
 import 'package:pbh_project/screens/donation_screen.dart';
 
 import 'package:pbh_project/screens/onboarding/onboarding_screen.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         cardColor: const Color.fromRGBO(191, 229, 192, 1),
       ),
       home: LoadingOverlay(
-        child: DonationScreen(),
+        child: OnBoardingScreen(),
       ),
     );
   }
